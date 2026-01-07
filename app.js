289caf42de3b172c1a6149fc44a191cb6670738e
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-07 12:57:08 +0500

    Initial commit

--- app.js
+++ app.js
@@ -33,3 +33,22 @@ function showDateTime() {
 
     let monName = months[now.getMonth()];
     // console.log(monName);
+
+//     let currentDate = now.getDate();
+//     let currentYear = now.getFullYear();
+
+//     document.getElementById("date").innerHTML = `${currentDate} ${monName}, ${dayName} ${currentYear}`;
+
+//     // let hours = now.getHours();
+
+//     let hours = now.getHours() % 13;
+//     let min = now.getMinutes();
+//     let sec = now.getSeconds();
+
+//     document.getElementById("time").innerHTML = `${hours}:${min}:${sec}`;
+
+// };
+
+// showDateTime();
+
+// setInterval(showDateTime, 1000);
