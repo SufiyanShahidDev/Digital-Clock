75d1fca2bed505fdf96094c2abee50b43af0df0e
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-07 12:55:46 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+// function showDateTime() {
+//     let now = new Date();
+//     // console.log(now);
+
+//     let days = [
+//         "Sun",
+//         "Mon",
+//         "Tues",
+//         "Wed",
+//         "Thur",
+//         "Fri",
+//         "Sat"
+//     ];
+
+//     let dayName = days[now.getDay()];
+//     // console.log(dayName);
+
+//     let months = [
+//         "Jan",
+//         "Feb",
+//         "Mar",
+//         "Apr",
+//         "May",
+//         "June",
+//         "July",
+//         "Aug",
+//         "Sep",
+//         "Oct",
+//         "Nov",
+//         "Dec"
+//     ];
+
+//     let monName = months[now.getMonth()];
+//     // console.log(monName);
